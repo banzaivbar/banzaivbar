ef13bfbd35f51018b63d1bed1e35d7f84979e79b
banzaivbar <102212942+banzaivbar@users.noreply.github.com>
2022-03-23 10:53:40 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+echo "# banzaivbar" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/banzaivbar/banzaivbar.git
+git push -u origin main
